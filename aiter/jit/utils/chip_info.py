f43c056ae1c0915fbeda381b8f4a79f69d53735f
hnhyzz <hnhyzz@qq.com>
2026-07-06 20:23:28 +0800

    Add support for gfx908 in chip_info.py

--- aiter/jit/utils/chip_info.py
+++ aiter/jit/utils/chip_info.py
@@ -449,6 +449,8 @@ def get_device_name():
         return "MI300"
     elif gfx == "gfx950":
         return "MI350"
+    elif gfx == "gfx908":
+        return "MI100"
     elif gfx == "gfx1250":
         return "MI400"
     else:
